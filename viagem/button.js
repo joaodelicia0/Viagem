d7c1daf2d8098d652401af24c8798023727a88af
joaodelicia0 <jpfb1@aluno.ifal.edu.br>
2025-04-16 00:08:55 -0300

    Add files via upload

---
+++ viagem/button.js
@@ -0,0 +1,8 @@
+   // Função de validação do formulário
+   function redirecionar(event) {
+    event.preventDefault();
+
+    // Se todas as validações passarem, redireciona para outra página
+    window.location.href = "index2.html"; 
+     // Substitua com a URL de/ Se tudo estiver certo, o formulário é "enviado"
+}
